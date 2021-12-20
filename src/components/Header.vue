9a9ae6f4bf77bfaf86c0e8d64a5e5abf6e24f7d1
RomeoAlex <romeo.alessandro17@gmail.com>
2021-12-20 17:56:36 +0100

    scaffolding rifatto

---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+    <header>
+        <div class="logo">
+            <img src="../img/dc-logo.png" alt="Logo DC">
+        </div>        
+    </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
