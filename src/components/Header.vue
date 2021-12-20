a17c889a15fc6904c178ae922829114ed6c821bb
RomeoAlex <romeo.alessandro17@gmail.com>
2021-12-20 18:27:01 +0100

    creata struttura da ta per i link della nav-bar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,30 +2,82 @@
     <header>
         <div class="logo">
             <img src="../img/dc-logo.png" alt="Logo DC">
-        </div>        
+        </div>
+
+        <div class="nav-bar">
+            <ul class="menu">
+                <li></li>
+            </ul>
+        </div> 
+          
     </header>
 </template>
 
 <script>
 export default {
   name: "Header",
-};
+  
+  data: function(){
+      return {
+          links: [
+      {        
+          text: 'CHARACTERS',
+          url:'#',
+          current: false,
+      },
+      {
+          text: 'COMICS',
+          url:'#',
+          current: true,
+      },
+      {
+          text: 'MOVEIS',
+          url:'#',
+          current: false,
+      },
+      {
+          text: 'TV',
+          url:'#',
+          current: false,
+      },
+      {
+          text: 'GAMES',
+          url:'#',
+          current: false,
+      },
+      {
+          text: 'COLLECTIBLES',
+          url:'#',
+          current: false,
+      },
+      {
+          text: 'VIDEOS',
+          url:'#',
+          current: false,
+      },
+      {
+          text: 'FANS',
+          url:'#',
+          current: false,
+      },
+      {
+          text: 'NEWS',
+          url:'#',
+          current: false,
+      },
+      {
+          text: 'SHOP',
+          url:'#',
+          current: false,
+      },
+    ],
+    }
+
+}
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
